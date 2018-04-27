d49f9b99c6e312dca935f8903f580220446e4130
Antonio Aloisio <antonio.aloisio@zalando.fi>
2018-04-27 22:43:51 +0300

    Worst commit of my life.. just copied over MaterialPainter here.. a lot of cleaning is needed to get it look nice as template..

--- __init__.py
+++ __init__.py
@@ -1,11 +1,37 @@
-from ui import panel
+import bpy
+import layers_view
+import helpers.updater as updater
 
-# Requires Python >= 3.4
-if "bpy" in locals():
-    from importlib import reload
-    if "panel" in locals():
-        reload(panel)
+bl_info = {
+    "name": "Material Painter",
+    "author": "Antonio Aloisio",
+    "version": (0, 0, 1),
+    "blender": (2, 7, 8),
+    "location": "3D View > Tool Shelve > Mixamo Tab",
+    "description": ("Experimental PBR Material painter"),
+    "warning": "",  # used for warning icon and text in addons panel
+    "wiki_url": "https://github.com/gnuton/MaterialPainter/wiki",
+    "tracker_url": "https://github.com/gnuton/MaterialPainter/issues",
+    "category": "Texturing"
+}
+
+
+def register():
+    layers_view.register()
+
+
+def unregister():
+    layers_view.unregister()
+
+
+# Add here the packages you wanna to be reloaded when the developer press "Run Script"
+pkgs = ["helpers", "libs", "materials_mgr", "layers_view"]
+if bpy.__name__ in locals():
+    import helpers.package_reloader as pr
+
+    pr.reload(pr)  # auto-reload itself
+    pr.PackageReloader().reload_packages(pkgs)
 
 if __name__ == "__main__":
-    print("Loading...")
-    panel.main()
+    updater.init(bl_info)
+    register()
--- ui/panel.py
+++ ui/panel.py
@@ -3,7 +3,7 @@ import bpy
 
 class HelloWorldPanel(bpy.types.Panel):
     """Creates a Panel in the Object properties window"""
-    bl_label = "Hello World Panel"
+    bl_label = "Hello World Panel5"
     bl_idname = "OBJECT_PT_hello"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
